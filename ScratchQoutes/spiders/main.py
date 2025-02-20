0c1f53c91f3351a9f1f2e3198df74b16cd43f61c
SouravSohal <contact.souravsohal@gmail.com>
2025-02-20 18:12:08 +0530

    Css-selectors-hands-on

--- ScratchQoutes/spiders/main.py
+++ ScratchQoutes/spiders/main.py
@@ -1,14 +1,27 @@
+# import scrapy
+
+# class ScratchQoutes(scrapy.Spider):
+#     name='quotes'
+#     start_urls=["https://quotes.toscrape.com/"]
+    
+#     def parse(self,response):
+#         print("\n\n\n\n\n----------------")
+#         print(f"The URL is {response.url}")
+#         print(f"The header is {response.headers.get('Content-Length')}")
+#         print(response.body.decode('utf-8'))
+#         print('\n\n\n\n\n\n\n')
+
+
 import scrapy
 
-class ScratchQoutes(scrapy.Spider):
-    name='quotes'
-    start_urls=["https://quotes.toscrape.com/"]
+class QuoteScraper(scrapy.Spider):
+    name="quotes"
+    start_urls=['https://quotes.toscrape.com/']
     
     def parse(self,response):
-        print("\n\n\n\n\n----------------")
-        print(f"The URL is {response.url}")
-        print(f"The header is {response.headers.get('Content-Length')}")
-        print(response.body.decode('utf-8'))
         
+        for quote in response.css('.text::text').getall():
+            yield{
+                'Quotes':quote
+            }
         
-        print('\n\n\n\n\n\n\n')
